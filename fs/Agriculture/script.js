0450549c1b151e24f3e64a49d83416f4e83b8f12
2batch34 <151673262+2batch34@users.noreply.github.com>
2023-11-22 00:18:06 +0530

    Add files via upload

---
+++ fs/Agriculture/script.js
@@ -0,0 +1,21 @@
+let cart = [];
+function addToCart(itemName, itemPrice) {
+    cart.push({ name: itemName, price: itemPrice });
+    updateCart();
+}
+function updateCart() {
+    const cartList = document.getElementById("cart-list");
+    const totalPriceElement = document.getElementById("total-price");
+  // Clear the cart list
+    cartList.innerHTML = "";
+   // Populate the cart list
+    let totalPrice = 0;
+    cart.forEach(item => {
+    const listItem = document.createElement("li");
+        listItem.textContent = `${item.name} - $${item.price.toFixed(2)}`;
+        cartList.appendChild(listItem);
+        totalPrice += item.price;
+    });
+    // Update the total price
+    totalPriceElement.textContent = `Total Price: $${totalPrice.toFixed(2)}`;
+}
